cfeeb639786849aac52810997cf2eba2a799c86b
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-10 22:33:33 +0530

    feat:NLogAddTwoNumbers

---
+++ Nlog/AddNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogSession
+{
+   public class AddNumber
+    {
+        private readonly NLog nlog = new NLog();
+        public int sum(int firstNumber, int secondNumber)
+        {
+            if(firstNumber == 0 || secondNumber == 0)
+            {
+                nlog.LogDebug("Debug Unsuccessfull : sum()");
+                nlog.LogError("Input numbrs cannot be zero");
+                nlog.LogWarn("firstNumber or secondNumber should not equal to zero");
+            }
+            int result = firstNumber + secondNumber;
+            nlog.LogDebug("Debug Successfull : sum()");
+            nlog.LogInfo("Sum Method Passed, Result : " + result);
+            return result;
+        }
+    }
+}
---
+++ Nlog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace NLogSession
+{
+  public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Addition problem \n Enter a,b values");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            AddNumber add = new AddNumber();
+            Console.WriteLine("Result is"+" " + add.sum(a , b));
+        }
+    }
+}
